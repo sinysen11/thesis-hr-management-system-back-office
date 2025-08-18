2ad673989bc43ee9e35f332263fd2acdaf04c08f
SINY SEN <siny.sen@student.passerellesnumeriques.org>
2025-08-18 11:57:45 +0700

    fixed

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ mongoose.connect(process.env.MONGO_URI, {
     console.log('MongoDB connected');
     // createDefaultAdmin();
     app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch(err => console.error(err));
